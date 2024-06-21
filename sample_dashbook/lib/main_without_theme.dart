b42e5b68705b97b25ce427f439f97b22ca6a9882
Kim Chi <45139505+dnkchi2809@users.noreply.github.com>
2024-06-21 17:16:50 +0700

    feat: add sample dashbook

--- sample_dashbook/lib/main_without_theme.dart
+++ sample_dashbook/lib/main_without_theme.dart
@@ -1,17 +1,10 @@
-
-
 import 'package:dashbook/dashbook.dart';
 import 'package:flutter/material.dart';
 import 'package:sample_dashbook/stories.dart';
 import 'package:sample_dashbook/text_story.dart';
 
 void main() {
-  final dashbook = Dashbook.dualTheme(
-    light: ThemeData(),
-    dark: ThemeData.dark(),
-    title: 'Dashbook Example',
-    autoPinStoriesOnLargeScreen: false,
-  );
+  final dashbook = Dashbook(autoPinStoriesOnLargeScreen: true);
 
   addTextStories(dashbook);
   addStories(dashbook);
